4223df8424713b5c4c6b4a5988cd3e2a8e0d099f
jasminkamble <jasminkamble2@gmail.com>
2026-06-25 14:46:54 +0000

    25.06.26

--- List/Removing_Elements_from_the_List.py
+++ List/Removing_Elements_from_the_List.py
@@ -1,8 +1,19 @@
-list_a =[34.6,67,"one",4+7j,"ram",67]
+list_a =[34.6,67,89,"one",4+7j,"ram",67]
 
 # remove(value): Removes the first occurrence of the value.
 list_a.remove(67)
-print("removing the firt occurrence of 67 fromthe list using remove fun^n\n",list_a)
+print("removing the firt occurrence of 67 from the list using remove fun^n\n",list_a)
 print("\n")
 
 # pop(index): Removes the element at a specific index (or the last element by default).
+list_a.pop()
+print("remove the last element by default\n",list_a)
+print("\n")
+
+list_b=[34.8,98,90,"one",4+9j,98]
+list_b.pop(3)
+print("Removes the element at a specific index\n",list_b)
+print("\n")
+
+del list_b[0]
+print("deliting the list")
---
+++ List/len_sort_reverse.py
@@ -0,0 +1,5 @@
+list_a =[78,69,7,84,45,26,12]
+
+no_of_ele =len(list_a)
+
+list_a.sort()
