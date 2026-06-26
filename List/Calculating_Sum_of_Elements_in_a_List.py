121620d285669251df3ba412ce69d416824288ff
jasminkamble <jasminkamble2@gmail.com>
2026-06-26 13:24:04 +0000

    26.06.26

---
+++ List/Calculating_Sum_of_Elements_in_a_List.py
@@ -0,0 +1,4 @@
+list_n =[64,72,62,51,26,49,78]
+print("given list:-",list_n)
+
+print("sum of the elements in list:-",sum(list_n))
---
+++ List/Finding_the_Largest_and_Smallest_Number_List.py
@@ -0,0 +1,5 @@
+list_n =[34,97,65,84,53,46,13,56,15]
+print("given list:-",list_n)
+
+print("the smallest number in the given list is:-",min(list_n))
+print("the largest number in the given list is:-",max(list_n))
