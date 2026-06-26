e4cfe516b97456c388ae0cc485383740f814fb9a
jasminkamble <jasminkamble2@gmail.com>
2026-06-26 13:07:57 +0000

    26.06.26

--- List/len_sort_reverse.py
+++ List/len_sort_reverse.py
@@ -1,5 +1,13 @@
 list_a =[78,69,7,84,45,26,12]
+print("Given list")
 
 no_of_ele =len(list_a)
+print("length of the list is :-",no_of_ele)
+
+
+list_a.reverse()
+print("reverse of the given list:-",list_a)
+
 
 list_a.sort()
+print("sorting the list:-",list_a)
