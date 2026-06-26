bb74aa6c628d0c72cba6c44e2f29f7943fcd4314
jasminkamble <jasminkamble2@gmail.com>
2026-06-26 14:01:58 +0000

    26.6.26

---
+++ List/Nested_list.py
@@ -0,0 +1,14 @@
+list_n=[[10,20,30],[40,50,60],[70,80,90]]
+print("Given list:-",list_n)
+
+# accession element of nested list
+print("list_n[0][0]:",list_n[0][0])
+print("list_n[0][1]:",list_n[0][1])
+print("list_n[0][2]:",list_n[0][2])
+print("list_n[1][0]:",list_n[1][0])
+print("list_n[1][1]:",list_n[1][1])
+print("list_n[1][2]:",list_n[1][2])
+print("list_n[2][0]:",list_n[2][0])
+print("list_n[2][1]:",list_n[2][1])
+print("list_n[2][2]:",list_n[2][2])
+
