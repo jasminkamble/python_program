22d2e8f80b5163d6565722efcb576dce1e3178e1
jasminkamble <jasminkamble2@gmail.com>
2026-06-30 14:28:56 +0000

    30.06.26

---
+++ dictionary/Removing_Items_from_a_Dictionary.py
@@ -0,0 +1,24 @@
+dict_x = {  
+    "name": "Sachin",   
+    "age": 18,   
+    "gender": "male",   
+    "profession": "student"  
+    }  
+print("Given dictionary:",dict_x)
+print("\n")
+
+print("del: This keyword is used to remove an item by key.")
+del dict_x["gender"]
+print("update dictoinary (remove gender)",dict_x)
+print("\n")
+
+print("pop(): This method is used to remove an item by key. It also returns the value of the removed item.")
+pop_age = dict_x.pop("age")
+print("update dictoinary(remove age)",dict_x)
+print("pop_age:-",pop_age)
+print("\n")
+
+print("popitem(): This method removes and returns the last 'key: value' pair.")
+poped_item =dict_x.popitem()
+print("update dictoinary(remove last item):-",dict_x)
+print("poped_item:-",poped_item)
