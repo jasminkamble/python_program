f72fcb881efe2e85527500d96eaf2895202b8269
jasminkamble <jasminkamble2@gmail.com>
2026-06-30 14:08:46 +0000

    30.06.26

---
+++ dictionary/Accessing_Dictionary_Items.py
@@ -0,0 +1,13 @@
+dict_a ={1:"one",2:"two",3:"three",4:"four",5:"five"}
+print("Given dictinary:-",dict_a)
+
+print("asseccing dictinary items by using [key]")
+print("1:",dict_a[1])
+print("2:",dict_a[2])
+
+
+print("asseccing dictinary items by using get(key)")
+print("3:",dict_a.get(3))
+print("4:",dict_a.get(4))
+print("5:",dict_a.get(5))
+
---
+++ dictionary/Adding_item_to_dictinary.py
@@ -0,0 +1,11 @@
+dict_x = {  
+    "name": "Sachin",   
+    "age": 18,   
+    "gender": "male",   
+    "profession": "student"  
+    }  
+
+print("Given Dictinary:-",dict_x)
+
+dict_x["ID"]=109
+print("update Dictinary (by adding ID:109)",dict_x)
