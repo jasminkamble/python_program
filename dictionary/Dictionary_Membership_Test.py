7fd8886246af308e80aff77f3e3fde88b8525ada
jasminkamble <jasminkamble2@gmail.com>
2026-07-01 13:55:46 +0000

    01.07.26

---
+++ dictionary/Dictionary_Membership_Test.py
@@ -0,0 +1,12 @@
+dict_x = {  
+    "name": "Sachin",   
+    "age": 18,   
+    "gender": "male",   
+    "profession": "student",  
+    "country": "India"  
+    }  
+print("Given dict:-",dict_x)
+
+print("Is \'age\' member of dict_x?",'age' in dict_x)
+print("Is \'age\' NOT member of dict_x?",'age' not in dict_x)
+print("Is \'country\' member of dict_x?",'country' in dict_x)
---
+++ dictionary/taking_input_in_dict.py
@@ -0,0 +1,7 @@
+dict_x
+
+for key in dict_x:
+    value =dict_x[key]
+    key=input(str("enter the key:-"))
+    value=input(str("enter the value:-"))
+    print(key,":",value)
