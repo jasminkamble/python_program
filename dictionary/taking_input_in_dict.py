e22df110090bea768333ccf13c1f44fe94ab9637
jasminkamble <jasminkamble2@gmail.com>
2026-07-01 14:27:51 +0000

    01.07.26

--- dictionary/taking_input_in_dict.py
+++ dictionary/taking_input_in_dict.py
@@ -1,7 +1,16 @@
-dict_x
+dict_x={}
 
-for key in dict_x:
-    value =dict_x[key]
+for i in range(2):
     key=input(str("enter the key:-"))
     value=input(str("enter the value:-"))
+    dict_x[key]=value
     print(key,":",value)
+
+print("dictionary :-",dict_x)
+
+# for key in dict_x:
+#     value = dict_x[key]
+dict_x.pop(key)
+print("after pop:-",dict_x)
+
+
