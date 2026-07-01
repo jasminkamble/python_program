2a65b1bdfa7d1d5d0a75e31a7eed2d9fdb5cfb36
jasminkamble <jasminkamble2@gmail.com>
2026-07-01 13:36:11 +0000

    01.07.26

---
+++ dictionary/Iterating_Through_a_Dictionary.py
@@ -0,0 +1,12 @@
+dict_x = {  
+    "Name": "Sachin",   
+    "Age": 18,   
+    "Gender": "Male",   
+    "Profession": "Student",  
+    "Country": "India"  
+    }  
+print("Given dictioner:-",dict_x)
+
+for key in dict_x:
+    value=dict_x[key]
+    print(key,":",value)
