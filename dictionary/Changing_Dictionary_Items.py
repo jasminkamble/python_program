993877e3b3b2e1bfcbb3e852be8332ed13552fa2
jasminkamble <jasminkamble2@gmail.com>
2026-07-01 13:31:32 +0000

    01.07.26

---
+++ dictionary/Changing_Dictionary_Items.py
@@ -0,0 +1,14 @@
+dict_x = {  
+    "name": "Sachin",   
+    "age": 18,   
+    "gender": "male",   
+    "profession": "student",  
+    "country": "India"  
+    }  
+  
+print("Given dictionary:-",dict_x)
+
+dict_x["name"]="Rohit"
+dict_x["age"]=23
+
+print("updated dictionary:-",dict_x)
