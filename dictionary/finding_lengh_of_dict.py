6a2019af5d6258dece27bc05d986d1e3c9ae0f9a
jasminkamble <jasminkamble2@gmail.com>
2026-07-01 13:41:32 +0000

    01.07.26

---
+++ dictionary/finding_lengh_of_dict.py
@@ -0,0 +1,10 @@
+dict_x = {  
+    "Name": "Sachin",   
+    "Age": 18,   
+    "Gender": "Male",   
+    "Profession": "Student",  
+    "Country": "India"  
+    }
+print("Given Dictionary:-",dict_x)
+
+print("size of dictionary:-",len(dict_x))
