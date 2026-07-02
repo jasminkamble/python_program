36eed4a4e7067c3d1e9c426f68586f98e33e1c48
jasminkamble <jasminkamble2@gmail.com>
2026-07-02 13:43:48 +0000

    02.07.26

---
+++ user_difine_Function/with_argument_without_return_value.py
@@ -0,0 +1,6 @@
+def multi(a,b):
+    print(a*b)
+
+
+multi(50,78)
+multi(5,6)
---
+++ user_difine_Function/without_argument_without_return_value.py
@@ -0,0 +1,12 @@
+def sum():
+    a=int(input("Enter a:-"))
+    b=int(input("enter b:-"))
+    print(a+b)
+
+sum()
+sum()
+
+def hello():
+    print("hello  student")
+
+hello()    
