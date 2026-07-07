b1f939309a132f246cc5f50a840d8d21bac548ad
jasminkamble <jasminkamble2@gmail.com>
2026-07-07 13:56:32 +0000

    07.07.26

---
+++ Built-in_Functions/bin_func.py
@@ -0,0 +1,8 @@
+a = 20
+print(bin(a))
+
+print(bin(56))
+print(bin(4))
+print(bin(7))
+print(bin(8))
+print(bin(1))
---
+++ Built-in_Functions/bool_func.py
@@ -0,0 +1,14 @@
+# The Python bool() converts a value to a Boolean (True or False) using the standard truth testing procedure.
+
+test1 = []  
+print(test1,'is',bool(test1))  
+test1 = [0]  
+print(test1,'is',bool(test1))  
+test1 = 0.0  
+print(test1,'is',bool(test1))  
+test1 = None  
+print(test1,'is',bool(test1))  
+test1 = True  
+print(test1,'is',bool(test1))  
+test1 = 'Easy string'  
+print(test1,'is',bool(test1))  
