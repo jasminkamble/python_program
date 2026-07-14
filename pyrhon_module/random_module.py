1321a1e2156bd4ff283db8396b15ec74b3c99be4
jasminkamble <jasminkamble2@gmail.com>
2026-07-14 12:48:12 +0000

    14.07.26

---
+++ pyrhon_module/random_module.py
@@ -0,0 +1,8 @@
+import random
+
+print("print ramdom integer bet^n 1 to 10",random.randint(1,10))
+print("print ramdom floating num bet^n 0 to 1",random.random())
+
+lst =[1,4,True,False,"one",[1,3,45,8]]
+
+print("print random choice from the list",random.choice(lst))
