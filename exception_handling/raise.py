e6062f454af59ce910b7774c77cb0feef4c5553f
jasminkamble <jasminkamble2@gmail.com>
2026-07-27 14:39:46 +0000

    27.07.26

---
+++ exception_handling/raise.py
@@ -0,0 +1,4 @@
+num = [2,6,3,9,0,1]
+
+if len(num)<=8:
+    raise Exception(f"Length of the given list must be less than or equal to 8 but is {len(num)}")  
---
+++ exception_handling/try_except.py
@@ -0,0 +1,11 @@
+ 
+b = ["one","two","three","four"]
+
+try:
+    for i in range(5):
+        print(f"i={i},b[i]={b[i]}")
+
+
+except:
+    print("index out of range")
+
