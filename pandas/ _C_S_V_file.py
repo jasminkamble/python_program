3f97c3cb8837b49b92fa2ac74fbdf8277a459aeb
jasminkamble <jasminkamble2@gmail.com>
2026-08-08 14:26:52 +0000

    08.08.26

--- pandas/ _C_S_V_file.py
+++ pandas/ _C_S_V_file.py
@@ -1,18 +1,22 @@
-# import pandas as pd
-# var = pd.read_csv("pandas/students.csv")
-# print(var)
 
-# print("\n")
-# print("\n")
-# print("\n")
-# print("\n")
+print("printting data from csv")
+import pandas as pd
+var = pd.read_csv("pandas/simple.csv")
+print(var)
 
-
-# import pandas as pd
-# var = pd.read_csv("pandas/students.csv")
-# print(var.to_string())
+print("\n")
+print("\n")
+print("\n")
+print("\n")
 
 
 import pandas as pd
+var = pd.read_csv("pandas/simple.csv")
+print(var.to_string())
 
-pd.option.display.max_rows=10
+
+import pandas as pd
+pd.options.display.max_rows=50
+var = pd.read_csv('pandas/simple.csv')
+print(var)
+
