29380c46f3698dbf35bb568e8e0aafff1a316676
jasminkamble <jasminkamble2@gmail.com>
2026-08-12 14:37:19 +0000

    12.08.26

--- pandas/cleaning_data/wrong_data.py
+++ pandas/cleaning_data/wrong_data.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('pandas/cleaning_data/json_file.json')
+df = pd.read_json('pandas/cleaning_data/json_file.json')
 
-# df.loc[7,'Duration'] = 45
+df.loc[106,'Duration'] = 45
 
-print(df.DataFrame())
+print(df.to_string())
+
