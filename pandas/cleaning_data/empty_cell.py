b17988d5f40bf9b0eaef080893df1da85c184647
jasminkamble <jasminkamble2@gmail.com>
2026-08-13 14:09:57 +0000

    13.08.26

--- pandas/cleaning_data/empty_cell.py
+++ pandas/cleaning_data/empty_cell.py
@@ -5,3 +5,18 @@ var = pd.read_json('pandas/cleaning_data/json_file.json')
 new_var = var.dropna()
 
 print(new_var.to_string())
+
+
+#Notice in the result that some rows have been removed (row 17, 27 and 91).
+
+#These rows had cells with empty values.
+
+
+import pandas as pd
+
+var = pd.read_json('pandas/cleaning_data/json_file.json')
+
+var.dropna(inplace=True)
+
+print(var.to_string())
+
