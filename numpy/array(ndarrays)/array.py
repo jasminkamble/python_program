c70013aaa91629719bd642c67c3ac821cd9d46e7
jasminkamble <jasminkamble2@gmail.com>
2026-08-21 14:44:45 +0000

    21.08.26

---
+++ numpy/array(ndarrays)/array.py
@@ -0,0 +1,21 @@
+# Use a list to create a NumPy array:
+
+import numpy as np
+var = np.array([1,2,3,4,5])
+print("list to numpy array(ndarray)",var)
+print(type(var))
+print("\n")
+
+
+# use a tuple to create a numpy array:
+
+import numpy as np
+var = np.array([1,2,3,4,5])
+print("tuple to numpy array(nparray)",var)
+print(type(var))
+print("\n")
+
+
+#dimensions in array
+
+import numpy as np 
---
+++ numpy/intro.py
@@ -0,0 +1,11 @@
+import numpy 
+var =  numpy.array([1,2,3,4,5])
+print(var)
+
+# numpy as np
+import numpy as np
+var1 = np.array([1,2,3,4,5])
+print(var1)
+
+#Checking NumPy Version
+print("numpy version",np.__version__)
