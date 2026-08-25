46e2777ae789896730478ac6ad694c703a751f75
jasminkamble <jasminkamble2@gmail.com>
2026-08-25 14:40:14 +0000

    25.08.26

---
+++ numpy/data_type.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+var = np.array([1,2,3,4])
+print("datad type of [1,2,3,4]:-",var.dtype)
+
+var1 = np.array(['a','b','c','frwegf'])
+print("array containing string ['a','b','c','f']:-",var1.dtype)
